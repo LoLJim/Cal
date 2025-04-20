a49d8f375c99fe96a902e5089757df17779542d6
LoLJim <dimascoyoyo@gmail.com>
2025-04-21 01:16:26 +0300

    second commit

---
+++ source/Addition.java
@@ -0,0 +1,19 @@
+public class Addition extends Operation
+{
+    public Addition(long num1, long num2)
+    {
+        super(num1, num2);
+    }
+
+    @Override
+    public void execute()
+    {
+        result = num1 + num2;
+    }
+
+    @Override
+    public String getOperationSymbol()
+    {
+        return "+";
+    }
+}
